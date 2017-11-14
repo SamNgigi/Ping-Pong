2f40f6fd55ec1fdccc4741f01c27f89495840e83
Sam Ngigi <sam.t.ngigi@gmail.com>
2017-11-14 18:26:22 +0300

    Added the Unlist

--- js/main.js
+++ js/main.js
@@ -2,13 +2,13 @@
 var ping_pong = function(userInput) {
   for (var i = 1; i <= userInput; i++) {
     if ((i%15)===0) {
-      $("#output").append("Ping-Pong!" + "<br>");
+      $("#output").append("<li>" + "Ping-Pong!");
     }else if ((i%3)===0) {
-      $("#output").append("Ping" + "<br>");
+      $("#output").append("<li>" +"Ping");
     }else if ((i%5)===0) {
-      $("#output").append("Pong" + "<br>");
+      $("#output").append("<li>" + "Pong" );
     }else {
-      $("#output").append(i + "<br>");
+      $("#output").append("<li>" + i);
     }
   }
 }
